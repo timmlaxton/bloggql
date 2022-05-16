04fb021f44fb57a2b371f5daacf5f682c5fd4647
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-05-16 14:52:21 +0100

    Added Categories component

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -13,7 +13,7 @@ const Categories = () => {
 
 
   return (
-    <div className='bg-white shadow-lg rounded-lg p-8 mb-12'>
+    <div className='bg-red-400 shadow-lg rounded-lg p-8 mb-12'>
     <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
       Categories
     </h3>
