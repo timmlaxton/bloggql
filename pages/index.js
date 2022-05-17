3b6f7a3191c5682d65eb1e5e8bed0683555029b1
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-05-17 10:37:20 +0100

    edited functionality

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,30 @@
-import Head from 'next/head'
-import {PostCard, Categories, PostWidget} from '../components'
-import {getPosts} from '../services'
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-
-
-const Home = ({posts}) => {
-
-
-
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Blog</title>
-        </Head>
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className='lg:col-span-8 col-span-1'>
-                  {posts.map((post) => <PostCard post={post.node} key={post.title}/> )}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
-          <PostWidget/>
-          <Categories/>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
         </div>
-        </div>
-  </div>
-  )
+      </div>
+    </div>
+  );
 }
 
+// Fetch data at build time
 export async function getStaticProps() {
-  const posts = (await getPosts()) || []
-
+  const posts = (await getPosts()) || [];
   return {
-    props: {posts}
-  }
+    props: { posts },
+  };
 }
-
-export default Home
