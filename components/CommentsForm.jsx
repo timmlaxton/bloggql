dc9f55b74029be085d334443aa4043ef52e7f711
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-06-01 11:10:33 +0100

    fixed bugs in comments form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,77 +1,106 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
-import{submitComment} from '../services'
+import {submitComment} from '../services'
 
-const CommentsForm = ({slug}) => {
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(null)
+  const commentEl = useRef()
+  const nameEl = useRef()
+  const emailEl = useRef()
+  const storeDataEl = useRef()
 
-const [error, setError] = useState(false);
-const [localStorage, setLocalStorage] = useState(null);
-const [showSuccessMessage, setShowSuccesMessage] = useState(false);
-const commentEl = useRef();
-const nameEl = useRef();
-const emailEl = useRef();
-const storeDataEl = useRef();
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem("name")
+    emailEl.current.value = window.localStorage.getItem("email")
+  })
 
-useEffect(() => {
-  nameEl.current.value = window.localStorage.getItem('name')
-  emailEl.current.value = window.localStorage.getItem('email')
-},[])
+  const handleCommentSubmisson = () => {
+    setError(false)
 
-const handleCommentSubmission = () => {
-  setError(false)
+    const {value: comment} = commentEl.current
+    const {value: name} = nameEl.current
+    const {value: email} = emailEl.current
+    const {checked: storeData} = storeDataEl.current
 
-  const {value: comment} = commentEl.current
-  const {value: name} = nameEl.current
-  const {value: email} = emailEl.current
-  const {checked: storeData} = storeDataEl.current
 
-  if(!comment || !name|| !email) {
-  setError(true)
-  return
-}
 
-const commentObj = {
-  name, email, comment, slug}
+    if(!comment || !name|| !email) {
+      setError(true);
+      return;
+    }
 
-  if(storeData) {
-    window.localStorage.setItem('name', name)
-    window.localStorage.setItem('email', email)
-  } else {
-    window.localStorage.removeItem('name', name)
-     window.localStorage.removeItem('email', email)
+    const commentObj = { name, email, comment, slug }
+
+    if(storeData) {
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('email', email)
+    } else {
+      window.localStorage.removeItem('name', name)
+      window.localStorage.removeItem('email', email)
+    }
+
+    submitComment(commentObj)
+    .then((res) => {
+      setShowSuccessMessage(true)
+      setTimeout(() => {
+        setShowSuccessMessage(false)
+      }, 3000)
+    })
   }
 
-  submitComment(commentObj).then(() => {
-    setShowSuccesMessage(true)
-    setTimeout(() => {
-      setShowSuccesMessage(false)
-    }, 3000)
-  })
-}
-
-return (
-  <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-    <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
-    <div className="grid grid-cols-1 gap-4 mb-4">
-      <textarea  className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="comment" placeholder="Comment" />
-    </div>
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
-      <input type="text"  className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Name" name="name" />
-      <input type="email"  className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Email" name="email" />
-    </div>
-    <div className="grid grid-cols-1 gap-4 mb-4">
+  return (
+    <div className="-b-12 mb-8 rounded-lg bg-white p-8 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Comments Form
+      </h3>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <textarea
+          ref={commentEl}
+          className="w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <input
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          type="text"
+          ref={nameEl}
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          type="text"
+          ref={emailEl}
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4">
       <div>
-        <input  type="checkbox" id="storeData" name="storeData" value="true" />
-        <label className="text-gray-500 cursor-pointer" htmlFor="storeData"> Save my name, email in this browser for the next time I comment.</label>
+        <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" value="true" />
+        <label className='text-gray-500 cursor-pointer ml-2' htmlFor='storeData'>Save my e-mail and name for the next time I comment</label>
+      </div>
+      </div>
+
+      {error && (
+        <p className="text-xs text-red-500">All fields are required.</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmisson}
+          className="ease inline-block transition duration-500 hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
+        >
+          Post Comment
+        </button>
+        {showSuccessMessage && <span className='text-xl float-right font-emibold mt-3 text-green-500'>Comment submitted for review</span>}
       </div>
     </div>
-    {error && <p className="text-xs text-red-500">All fields are mandatory</p>}
-    <div className="mt-8">
-      <button onClick={handleCommentSubmission} type="button"  className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Post Comment</button>
-      {showSuccessMessage && <span className="text-xl float-right font-semibold mt-3 text-green-500">Comment submitted for review</span>}
-    </div>
-  </div>
-);
+  )
 }
 
 export default CommentsForm
